7b8d1d89ebdd4b0b777c961b7ea812edec89f582
Samar Ramireddy <samar.ramireddy@ggktech.com>
2021-01-11 17:45:35 +0530

    Add Purchase Details

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,6 +1,6 @@
 class Product
-  MILK = { :unit_price => 3.97, :min_sale_qty => 2, :sale_price => 5 }.freeze
-  BREAD = { :unit_price => 2.17, :min_sale_qty => 3, :sale_price => 6 }.freeze
+  MILK = { :unit_price => 3.97, :sale_quantity => 2, :sale_price => 5 }.freeze
+  BREAD = { :unit_price => 2.17, :sale_quantity => 3, :sale_price => 6 }.freeze
   BANANA = { :unit_price => 0.99 }.freeze
   APPLE = { :unit_price => 0.89 }.freeze
 end
@@ -10,8 +10,23 @@ class Purchase
     @items = {}
     data.uniq.each { |item| @items[item] = data.count(item) }
   end
+
+  def description
+    puchase_details = "Item     \tQuantity      Price\n--------------------------------------\n"
+    @items.each { |item, quantity| puchase_details += "#{item}\t\t#{quantity}\t\t#{sale_price(item, quantity)}\n" }
+    puchase_details
+  end
+
+  def sale_price(item, quantity)
+    item = Product.const_get(item.upcase)
+    sale_quantity = item[:sale_quantity] ? item[:sale_quantity] : 1
+    item_sale_price = item[:sale_price] ? item[:sale_price] : item[:unit_price]
+    (item_sale_price * (quantity / sale_quantity) + item[:unit_price] * (quantity % sale_quantity)).round(2)
+  end
 end
 
 puts "Please enter all the items purchased separated by a comma"
 data = gets.chomp.split(',').map{ |input| input.strip.capitalize }
 purchase = Purchase.new(data)
+
+puts purchase.description
